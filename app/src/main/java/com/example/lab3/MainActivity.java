d3fce908c78df0aabbdd150d7ece0817f55e042a
Chiến Trần <116490257+tcc3281@users.noreply.github.com>
2024-03-06 22:19:13 +0700

    sort, delete

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,8 +16,11 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         init();
         initData();
         setBtnAdd();
+        setBtnDelete();
     }
     protected void initData(){
         contacts.add(new Contact(1,"Chien Tran","093738943",false));
@@ -69,7 +74,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    protected void setBtnDelete(){
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Stack<Contact> st=new Stack<>();
+                for(int i =0;i<contacts.size();i++){
+                    Contact temp=contacts.get(i);
+                    if(temp.isStatus()){
+                        st.push(temp);
+                    }
+                }
+                while(!st.empty()){
+                    contacts.remove(st.pop());
+                }
+                adapter.notifyDataSetChanged();
+                Toast.makeText(MainActivity.this, "Đã xóa", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -96,14 +119,12 @@ public class MainActivity extends AppCompatActivity {
             adapter.notifyDataSetChanged();//error
         }
     }
-
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = new MenuInflater(this);
         inflater.inflate(R.menu.contextmenu,menu);
     }
-
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         Contact contact=contacts.get(selectedIndex);
@@ -117,7 +138,9 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtras(bundle);
             startActivityForResult(intent,103);
         } else if(item.getItemId()==R.id.menu_context_delete){
-
+            contacts.remove(selectedIndex);
+            adapter.notifyDataSetChanged();
+            Toast.makeText(this, "Đã xóa", Toast.LENGTH_SHORT).show();
         } else if(item.getItemId()==R.id.menu_context_call) {
             Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+contact.getPhone()));
             startActivity(intent);
@@ -127,5 +150,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=new MenuInflater(this);
+        inflater.inflate(R.menu.actionmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.menu_action_id){
+            Collections.sort(contacts,(u,v)->{
+                if(u.getId()>v.getId()) return 1;
+                else if (u.getId()==v.getId()) return 0;
+                else return -1;
+            });
+            adapter.notifyDataSetChanged();
+        } else if (item.getItemId()==R.id.menu_action_name) {
+            Collections.sort(contacts,(u,v)->{
+                return u.getFullname().compareTo(v.getFullname());
+            });
+            adapter.notifyDataSetChanged();
+        } else if (item.getItemId()==R.id.menu_action_phone) {
+            Collections.sort(contacts,(u,v)->{
+                return u.getPhone().compareTo(v.getPhone());
+            });
+            adapter.notifyDataSetChanged();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
